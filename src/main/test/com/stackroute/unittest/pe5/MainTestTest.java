b521303a2e78a93d28b327388951a4f0e22eb547
ujjawala.yati <ujjawala.yati@boeing.com>
2019-01-03 11:32:46 +0530

    PE5

--- src/main/test/com/stackroute/unittest/pe5/MainTestTest.java
+++ src/main/test/com/stackroute/unittest/pe5/MainTestTest.java
@@ -1,18 +1,25 @@
 package com.stackroute.unittest.pe5;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertEquals;
 
 public class MainTestTest {
-    private MainTest mainTest = new MainTest();
-    @Test
-    public void sortStudent() {
-        List<Student> expected = new ArrayList<>();
+    private MainTest mainTest;
+    private List<Student> expected;
+
+    @Before
+    public void setUp() throws Exception {
+        mainTest = new MainTest();
+        expected = new ArrayList<>();
         Student student1 = new Student();
         student1.setName("ujjawala");
         student1.setAge(24);
@@ -33,9 +40,24 @@ public class MainTestTest {
         student4.setAge(25);
         student4.setId(4);
         expected.add(student4);
+    }
+
+    @Test
+    public void sortStudent() {
         Collections.sort(expected, new StudentSorter());
-        assertEquals(expected, mainTest.sortStudent());
-//        Assert.assertThat(mainTest.sortStudent(), is(expected));
+        assertEquals(expected.toString(), mainTest.sortStudent().toString());
 //        assertThat(mainTest.sortStudent(), is(4));
+    }
+
+    @Test
+    public void sortStudentSize() {
+        Collections.sort(expected, new StudentSorter());
+//        assertEquals(expected.toString(), mainTest.sortStudent().toString());
+        Assert.assertThat(mainTest.sortStudent().size(), is(4));
+    }
+
+    @Test
+    public void sortStudentNotNull() {
+        assertNotNull(mainTest.sortStudent().toString());
     }
 }
